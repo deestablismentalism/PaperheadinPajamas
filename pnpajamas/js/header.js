bdde50294d44c0d4f74db187276dc8af9d53409c
deestablismentalism <132768127+deestablismentalism@users.noreply.github.com>
2024-09-25 00:32:41 +0800

    Add files via upload

--- pnpajamas/js/header.js
+++ pnpajamas/js/header.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(){
-    const menu = document.querySelector(".header-nav-bar");
-    const menuItems = document.querySelectorAll(".header-nav-bar li");
-    menu.addEventListener("touchstart", menuItems.classList.toggle("touched"));
+    let menuBtn = document.querySelector(".nav-bar-btn");
+    let menuItems = document.querySelectorAll(".header-nav-bar-mobile");
+    
+    let isOpen = false;
+   
+    menuBtn.onclick(function (){
+
+        if(!isOpen) {
+            menuItems.style.display = "block";
+            menuBtn.style.backgroundPosition = "center left 50px, center";
+            isOpen = true;
+        }
+        if(isOpen) {
+            menuItems.style.display = "none";
+            menuBtn.style.backgroundPosition = "center, center left 50px";
+            isOpen = false;
+        }
+    });
 });
