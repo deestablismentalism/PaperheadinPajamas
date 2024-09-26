3feba3608c184032ba26f378da7fb36465cf2031
deestablismentalism <132768127+deestablismentalism@users.noreply.github.com>
2024-09-26 21:30:37 +0800

    Add files via upload

--- pnpajamas/js/header.js
+++ pnpajamas/js/header.js
@@ -1,20 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(){
+
     let menuBtn = document.querySelector(".nav-bar-btn");
-    let menuItems = document.querySelectorAll(".header-nav-bar-mobile");
+    let menuItems = document.querySelector(".header-nav-bar-mobile");
     
     let isOpen = false;
    
-    menuBtn.onclick(function (){
+    menuBtn.onclick = function (){
 
         if(!isOpen) {
             menuItems.style.display = "block";
             menuBtn.style.backgroundPosition = "center left 50px, center";
             isOpen = true;
         }
-        if(isOpen) {
+        else if(isOpen) {
             menuItems.style.display = "none";
             menuBtn.style.backgroundPosition = "center, center left 50px";
             isOpen = false;
         }
-    });
+    }
 });
